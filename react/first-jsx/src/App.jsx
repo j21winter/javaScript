c9510ff7d117992f272adee8c26d95559c935245
j21winter <j21winter@gmail.com>
2023-10-26 16:36:26 -0400

    first JSX

---
+++ react/first-jsx/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h4>Things I need to do:</h4>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
