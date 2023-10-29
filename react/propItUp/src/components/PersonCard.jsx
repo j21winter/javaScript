a74ac743cd292e42c6dc51922f8588e9b61d64ad
j21winter <j21winter@gmail.com>
2023-10-28 21:02:25 -0400

    useState

--- react/propItUp/src/components/PersonCard.jsx
+++ react/propItUp/src/components/PersonCard.jsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React, {useState} from "react";
 
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor } = props
+    const [currentAge, setCurrentAge] = useState(age);
     return (
         <div>
             <h1>{ firstName } { lastName }</h1>
-            <p>Age: { age }</p>
+            <p>Age: {  currentAge }</p>
             <p>Hair Color: { hairColor }</p>
+            <button onClick={ ( event ) => setCurrentAge( currentAge + 1 ) }>Birthday button for { firstName } { lastName }</button>
         </div>
     );
 }
 
-export default PersonCard;
+export default PersonCard; 
