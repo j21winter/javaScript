0333c1dbbb3cd7b33479b3b9d22e6ad9b9d509e8
j21winter <j21winter@gmail.com>
2023-11-07 22:12:20 -0500

    API with Axios

---
+++ APIs/axiosPokeminApi/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import Pokemon from './components/Pokemon'
+
+function App() {
+
+  return (
+    <>
+      <Pokemon/>
+    </>
+  )
+}
+
+export default App
