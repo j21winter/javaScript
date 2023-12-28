95bfcb28043b81ef9437b261bbb65f625f18c940
j21winter <j21winter@gmail.com>
2023-12-27 19:48:03 -0600

    state

--- advancedMernConcepts/bigInversion/src/components/PersonCard.jsx
+++ advancedMernConcepts/bigInversion/src/components/PersonCard.jsx
@@ -1,14 +1,21 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            age : this.props.age
+        }
+    }
     render(){
         const {firstName, lastName, age, hairColor} = this.props;
 
         return(
             <div>
                 <h1>{firstName} {lastName}</h1>
-                <p>{age}</p>
+                <p>{this.state.age}</p>
                 <p>{hairColor}</p>
+                <button onClick={() => {this.setState({ age : (this.state.age += 1) })}}>Birthday Button for {firstName} {lastName} </button>
             </div>
         )
         
